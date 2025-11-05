80730c4cd8abf6250515a9b15cab54fa4659a7bc
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-05 15:09:05 +0100

    feat: esercizio completato

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const bottone = document.querySelector(".button")
 const lampadina = document.querySelector(".bulbo")
 
 bottone.addEventListener("click", function(){
-bottone.innerHTML= "miaomiao"
-lampadina.src= "" 
-
-
+bottone.innerHTML= "spegni"
+lampadina.src= 'img (4)/img/yellow_lamp.png'
 
 })
