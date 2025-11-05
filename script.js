d7ee4b693699f38501f245ae400654686214b16a
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-05 15:29:50 +0100

    feat: parte del bonus fatta ma non ho creatività lol

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const bottone = document.querySelector(".button")
 const lampadina = document.querySelector(".bulbo")
-
+let acceso = false
 bottone.addEventListener("click", function(){
-bottone.innerHTML= "spegni"
-lampadina.src= 'img (4)/img/yellow_lamp.png'
+if(acceso=== false){
+  bottone.innerHTML= "spegni";
+  lampadina.src= 'img (4)/img/yellow_lamp.png'
+acceso=true;
+}
+else{
+bottone.innerHTML="accendi"
+lampadina.src="img (4)/img/white_lamp.png"
+acceso = false;
+
+}
+
 
 })
