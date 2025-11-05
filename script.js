75d69a52d11b28250f59263dd0e939c3c6c8b60f
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-05 15:02:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const bottone = document.querySelector(".button")
+const lampadina = document.querySelector(".bulbo")
+
+bottone.addEventListener("click", function(){
+bottone.innerHTML= "miaomiao"
+lampadina.src= "" 
+
+
+
+})
