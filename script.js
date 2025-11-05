2b5b48ca4af3a534dd1680e8facfbe21cb273761
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-05 16:46:36 +0100

    feat: fatta la parte dello stile del bonus

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ acceso = false;
 
 
 })
+const bottoneSecond = document.querySelector(".button-2")
+const lampadinaSecond = document.querySelector(".bulbo-2")
+let accesoSecond = false
+bottoneSecond.addEventListener("click", function(){
+if(acceso=== false){
+  bottoneSecond.innerHTML= "spegni";
+  lampadinaSecond.src= 'img (4)/img/yellow_lamp.png'
+accesoSecond=true;
+}
+else{
+bottoneSecond.innerHTML="accendi"
+lampadinaSecond.src="img (4)/img/white_lamp.png"
+accesoSecond = false;
+
+}
+
+
+})
