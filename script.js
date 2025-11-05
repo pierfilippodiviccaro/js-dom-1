8fcce138d2f0f8e93c8b2f790d90bb2a183bdf09
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-05 17:14:56 +0100

    feat: sistemata e ottimizzata parte del codice

--- script.js
+++ script.js
@@ -1,36 +1,11 @@
-const bottone = document.querySelector(".button")
-const lampadina = document.querySelector(".bulbo")
-let acceso = false
-bottone.addEventListener("click", function(){
-if(acceso=== false){
-  bottone.innerHTML= "spegni";
-  lampadina.src= 'img (4)/img/yellow_lamp.png'
-acceso=true;
+const buttons = document.querySelectorAll("[data-button]");
+
+for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.addEventListener('click', function() {
+        const idLamp = button.getAttribute('data-button');
+        const lamp = document.querySelector('[data-lamp="' + idLamp + '"]');
+        button.innerHTML= "spegni";
+        lamp.src= 'img (4)/img/yellow_lamp.png'
+    });
 }
-else{
-bottone.innerHTML="accendi"
-lampadina.src="img (4)/img/white_lamp.png"
-acceso = false;
-
-}
-
-
-})
-const bottoneSecond = document.querySelector(".button-2")
-const lampadinaSecond = document.querySelector(".bulbo-2")
-let accesoSecond = false
-bottoneSecond.addEventListener("click", function(){
-if(acceso=== false){
-  bottoneSecond.innerHTML= "spegni";
-  lampadinaSecond.src= 'img (4)/img/yellow_lamp.png'
-accesoSecond=true;
-}
-else{
-bottoneSecond.innerHTML="accendi"
-lampadinaSecond.src="img (4)/img/white_lamp.png"
-accesoSecond = false;
-
-}
-
-
-})
